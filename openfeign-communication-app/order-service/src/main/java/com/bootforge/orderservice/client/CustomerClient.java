b968e6b4748fc4108dba27eb26363ac45334554d
bootforge-dev <themadhavp94@gmail.com>
2026-09-19 12:04:04 +0530

    feat(order-service): configure OpenFeign timeouts and retries

--- openfeign-communication-app/order-service/src/main/java/com/bootforge/orderservice/client/CustomerClient.java
+++ openfeign-communication-app/order-service/src/main/java/com/bootforge/orderservice/client/CustomerClient.java
@@ -1,11 +1,15 @@
 package com.bootforge.orderservice.client;
 
 import com.bootforge.commons.dto.customerservice.CustomerResponse;
+import com.bootforge.orderservice.config.FeignRetryConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "customer-service")
+@FeignClient(
+        name = "customer-service",
+        configuration = FeignRetryConfig.class
+)
 public interface CustomerClient {
 
     @GetMapping("/api/v1/customers/{id}")
