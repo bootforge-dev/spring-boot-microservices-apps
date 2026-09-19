af71faee3f26a8c3c36a0dbe5a556ba94c85038d
bootforge-dev <themadhavp94@gmail.com>
2026-09-19 12:19:51 +0530

    feat(inventory): add inventory service with Kafka consumer

---
+++ openfeign-communication-app/inventory-service/src/main/java/com/bootforge/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,10 @@
+package com.bootforge.inventoryservice.repository;
+
+import com.bootforge.inventoryservice.entity.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    Optional<Inventory> findByProductId(Long productId);
+}
